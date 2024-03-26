90d4637a91352033ab92b8d854e7bf77a2725470
npranto <npranto@gmail.com>
2024-03-25 21:02:56 -0400

    cleans up commented code

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -27,18 +27,6 @@ export default function NavBar() {
               <span className="badge badge-sm indicator-item">8</span>
             </div>
           </div>
-          {/* <div
-            tabIndex={0}
-            className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
-          >
-            <div className="card-body">
-              <span className="font-bold text-lg">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
-              </div>
-            </div>
-          </div> */}
         </div>
         <div className="dropdown dropdown-end">
           <div
