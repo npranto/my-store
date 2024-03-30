be4125ffbc99dfd7141eb5218172706955684a56
npranto <npranto@gmail.com>
2024-03-29 21:15:46 -0400

    adds ui for product page

---
+++ pages/product/[slug].js
@@ -0,0 +1,186 @@
+import React from "react";
+
+export default function Product() {
+  return (
+    <div className=" py-8">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col md:flex-row -mx-4">
+          <div className="md:flex-1 px-4">
+            <div className="w-full md:w-auto h-auto md:h-[460px] md:max-w-[60vw] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
+              <img
+                className="w-full h-full object-cover rounded-md"
+                src="https://cdn.pixabay.com/photo/2020/05/22/17/53/mockup-5206355_960_720.jpg"
+                alt="Product Image"
+              />
+            </div>
+            <div className="mb-4">
+              <div class="grid grid-cols-5 md:grid-cols-5 gap-2">
+                <div>
+                  <img
+                    class="h-auto max-w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    class="h-auto max-w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    class="h-auto max-w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    class="h-auto max-w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    class="h-auto max-w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    class="h-auto max-w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    class="h-auto max-w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    class="h-auto max-w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    class="h-auto max-w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    class="h-auto max-w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    class="h-auto max-w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    class="h-auto max-w-full rounded-lg"
+                    src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="md:flex-1 px-4">
+            <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">
+              Rainy Jacket
+            </h2>
+            <p className="text-gray-600 dark:text-gray-300 text-sm mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
+              ante justo. Integer euismod libero id mauris malesuada tincidunt.
+            </p>
+            <div className="flex items-center mb-4">
+              <div className="mr-4">
+                <span className="text-gray-600 font-bold text-3xl dark:text-gray-300">
+                  $29.99
+                </span>
+              </div>
+              <div className="badge text-white badge-success">In Stock</div>
+            </div>
+            <div className="mb-4 mt-8">
+              <span className="font-bold text-gray-700 dark:text-gray-300">
+                Color:
+              </span>
+              <div className="flex items-center mt-2">
+                <button className="w-6 h-6 rounded-full bg-gray-800 dark:bg-gray-200 mr-2"></button>
+                <button className="w-6 h-6 rounded-full bg-red-500 dark:bg-red-700 mr-2"></button>
+                <button className="w-6 h-6 rounded-full bg-blue-500 dark:bg-blue-700 mr-2"></button>
+                <button className="w-6 h-6 rounded-full bg-yellow-500 dark:bg-yellow-700 mr-2"></button>
+              </div>
+            </div>
+            <div className="mb-4">
+              <span className="font-bold text-gray-700 dark:text-gray-300">
+                Size:
+              </span>
+              <div className="flex items-center mt-2">
+                <button className="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">
+                  S
+                </button>
+                <button className="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">
+                  M
+                </button>
+                <button className="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">
+                  L
+                </button>
+                <button className="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">
+                  XL
+                </button>
+                <button className="bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white py-2 px-4 rounded-full font-bold mr-2 hover:bg-gray-400 dark:hover:bg-gray-600">
+                  XXL
+                </button>
+              </div>
+            </div>
+            <div className="mb-4">
+              <span className="font-bold text-gray-700 dark:text-gray-300">
+                Description:
+              </span>
+              <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
+                ante justo. Integer euismod libero id mauris malesuada
+                tincidunt. Vivamus commodo nulla ut lorem rhoncus aliquet. Duis
+                dapibus augue vel ipsum pretium, et venenatis sem blandit.
+                Quisque ut erat vitae nisi ultrices placerat non eget velit.
+                Integer ornare mi sed ipsum lacinia, non sagittis mauris
+                blandit. Morbi fermentum libero vel nisl suscipit, nec tincidunt
+                mi consectetur.
+              </p>
+            </div>
+            <div className="flex mb-4">
+              <div className="w-1/2 px-2">
+                <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">
+                  Add to Cart
+                </button>
+              </div>
+              <div className="w-1/2 px-2">
+                <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">
+                  Add to Wishlist
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
