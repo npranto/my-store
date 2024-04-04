7be947045bb95658470d99ededed14d21f45b291
npranto <npranto@gmail.com>
2024-04-03 20:18:26 -0400

    adds order confirmation page

---
+++ components/cancel-order-btn/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CancelOrderBtn() {
+  return (
+    <button class="rounded-full py-3 px-7 font-semibold text-sm leading-7 text-white bg-gray-600 max-lg:mt-5 shadow-sm shadow-transparent transition-all duration-500 hover:bg-gray-700 hover:shadow-gray-400">
+      Cancel Order
+    </button>
+  );
+}
