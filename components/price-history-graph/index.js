df6b588397102c51a9ed604495ecff6017bccaa6
npranto <npranto@gmail.com>
2024-04-28 02:43:23 -0400

    creates a graph of price history within each product page for users to be more aware of deals

---
+++ components/price-history-graph/index.js
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Area,
+  AreaChart,
+} from "recharts";
+
+const data = [
+  { month: "Apr", price: 110 },
+  { month: "May", price: 115 },
+  { month: "Jun", price: 118 },
+  { month: "Jul", price: 33 },
+  { month: "Aug", price: 130 },
+  { month: "Sep", price: 234 },
+  { month: "Oct", price: 140 },
+  { month: "Nov", price: 313 },
+  { month: "Dec", price: 150 },
+  { month: "Jan", price: 155 },
+  { month: "Feb", price: 130 },
+  { month: "Mar", price: 95 },
+];
+
+const capitalizeFirstLetterOfEachWord = (str) => {
+  return str.replace(/\w\S*/g, (txt) => {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+};
+
+export default function PriceHistoryGraph() {
+  const tooltipFormatter = (value, name) => {
+    return [`${capitalizeFirstLetterOfEachWord(name)}: $${value}`];
+  };
+
+  return (
+    <section className="py-4 w-full">
+      <h2 className="font-manrope font-bold text-2xl text-black mb-8 text-left">
+        Price History
+      </h2>
+      <AreaChart
+        className="max-w-[100%] min-h-[175px]"
+        width={700}
+        height={250}
+        data={data}
+        margin={{ top: 10, right: 10, left: 0, bottom: 10 }}
+      >
+        <defs>
+          <linearGradient id="colorPrice" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="month" />
+        <YAxis dataKey="price" />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip
+          formatter={tooltipFormatter}
+          itemStyle={{ color: "gray", fontWeight: "lighter" }}
+        />
+        <Area
+          type="monotone"
+          dataKey="price"
+          stroke="#82ca9d"
+          fillOpacity={1}
+          fill="url(#colorPrice)"
+        />
+      </AreaChart>
+    </section>
+  );
+}
